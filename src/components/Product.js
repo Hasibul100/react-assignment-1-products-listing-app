9f56bc312578a46c8d0c36e13f8c6d50077323ce
hasibbay <g143pay@gmail.com>
2024-07-08 23:14:19 +0600

    Fix ESLint errors and update Browserslist

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Product = () => {
+const Product = (props) => {
+  const {id ,rating,title,price,desc,img} = props;
   return (
-      <article className="product">
-      <img src="" alt="" />
+      <article className="product" key={id}>
+      <img src={img} alt="" />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{title}</h4>
+        <p>Price: $ {price}</p>
+        <p>Rating: {rating}/5</p>
+        <p className="product__desc">{desc}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,14 @@
 /* eslint-disable react/prop-types */
+import React from "react";
+ import Product from "./Product";
+const  Products = (props)=>{
+    const {data}= props;
+    return(
+        <div className="products">
+       { data.map((itm,ind)=><Product rating={itm.rating.rate} key={ind} price={itm.price} img={itm.image}  title={itm.title} desc={itm.description} />)}
+       
+     
+        </div>
+    )
+}
+export default Products;
