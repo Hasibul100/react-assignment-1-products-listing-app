c03174655ddc21b909f77739ffb9af7db05ea574
hasibbay <g143pay@gmail.com>
2024-07-09 21:32:36 +0600

    9/7/2024 update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,20 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 
 const Product = (props) => {
-  const {id ,rating,title,price,desc,img} = props;
+  const {ProductsData}=props;
+  const { id, rating, title, price, description, image } = ProductsData;
   return (
-      <article className="product" key={id}>
-      <img src={img} alt="" />
+    <article className="product" >
+      <img src={image} alt="" />
       <div className="product__details">
         <h4 className="product__title">{title}</h4>
-        <p>Price: $ {price}</p>
-        <p>Rating: {rating}/5</p>
-        <p className="product__desc">{desc}</p>
+        <p className="product__price"> Price: $ {price}</p>
+        <p className="product__rating">Rating: {rating.rate}/5</p>
+        <p className="product__desc">{description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
+      {console.log(ProductsData)}
     </article>
   );
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 import React from "react";
- import Product from "./Product";
-const  Products = (props)=>{
-    const {data}= props;
-    return(
-        <div className="products">
-       { data.map((itm,ind)=><Product rating={itm.rating.rate} key={ind} price={itm.price} img={itm.image}  title={itm.title} desc={itm.description} />)}
-       
-     
-        </div>
-    )
-}
+import Product from "./Product";
+const Products = (props) => {
+  const { data } = props;
+  return (
+    <div className="products">
+      {data.map((itm) => (
+        <Product key={itm.id} ProductsData={itm} />
+      ))}
+    </div>
+  );
+};
 export default Products;
